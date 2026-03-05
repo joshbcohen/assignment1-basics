a5c54eb7342950f119cf18b5fd5cd1bdbd51c83a
Miguel <miguelconner4@gmail.com>
2026-03-05 17:23:56 -0500

    try rms norm

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -54,3 +54,38 @@ class Embedding(torch.nn.Module):
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.embedding_matrix[token_ids]
+    
+class RMSNorm(torch.nn.Module):
+
+    def __init__(self, d_model:int, eps: float = 1e-5, device=None, dtype=None):
+        self.d_model = d_model
+        self.eps = eps
+
+        self.g = torch.empty(self.d_model)
+        std = (2 / (self.d_model)) ** (0.5)
+        torch.nn.init.trunc_normal_(
+            tensor=self.g,
+            mean=0,
+            std=std,
+            a=-3 * std,
+            b=3 * std,
+        )
+        self.gain = torch.nn.Parameter(self.g)
+
+    def _rms(self, x):
+        to_sum = x.pow(2) + self.eps
+        to_mul = to_sum.sum(dim=-1, keep_dim=True)
+        to_sqrt = torch.div(to_mul, self.d_model)
+        return to_sqrt.sqrt()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+
+        # code for RMSNorm
+        rms = self._rms()
+        first_result = torch.mul(self.gain, x)
+        result = torch.div(first_result,rms)
+        # x is (batch_size, seq_len, d_model), output is same
+
+        return result.to(in_dtype)
