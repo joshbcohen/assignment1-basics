e8572ebb77190c67cb4ee8c73d2be4ef845cf1fc
Joshua Cohen <jbcohen42@gmail.com>
2026-03-09 16:24:12 -0400

    Add RoPE adapter

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,7 +11,7 @@ from torch import Tensor
 
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.tokenizer import Tokenizer
-from cs336_basics.transformer import Linear, Embedding, RMSNorm, SwiGLU
+from cs336_basics.transformer import Linear, Embedding, RMSNorm, SwiGLU, RoPE
 
 
 def run_linear(
@@ -213,7 +213,7 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    return RoPE(theta, d_k, max_seq_len).forward(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
