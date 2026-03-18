5ecbc555d9d91c15a77d5d673b04e18b38924222
Joshua Cohen <jbcohen42@gmail.com>
2026-03-18 13:48:01 -0400

    Add some more functionality to training script

--- cs336_basics/main.py
+++ cs336_basics/main.py
@@ -1,17 +1,26 @@
 import argparse
+from uuid import uuid4
+import logging
+
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
 from transformer import TransformerLM
 from support import cross_entropy
 from optimizer import AdamW, get_learning_rate_schedule, apply_gradient_clipping
 from training_loop import data_loading, save_checkpoint, load_checkpoint
+import wandb
+
+logger = logging.getLogger(__name__)
+
 
 def main():
-
     parser = argparse.ArgumentParser(description="Train transformer model.")
 
     parser.add_argument("--device", type=str, default="cpu", help='Device: "cpu", "mps" (Mac), or "cuda"')
 
     # data loading
-    parser.add_argument("dataset", type=str, help="Provide training dataset.")
+    parser.add_argument("dataset", type=str, help="Path to training dataset. Should be pre-tokenized")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size.")
     parser.add_argument("--context_length", type=int, default=256, help="Context length of LM.")
 
@@ -24,7 +33,9 @@ def main():
     parser.add_argument("--d_model", type=int, default=512, help="Dimensions of model.")
     parser.add_argument("--num_layers", type=int, default=4, help="Number of layers for Transformer.")
     parser.add_argument("--num_heads", type=int, default=16, help="Number of heads for Transformer.")
-    parser.add_argument("--d_ff", type=int, default=1344, help="Should usually be a multiple of 64, and around 3/8 * d_model.")
+    parser.add_argument(
+        "--d_ff", type=int, default=1344, help="Should usually be a multiple of 64, and around 3/8 * d_model."
+    )
     parser.add_argument("--theta", type=int, default=1000, help="Theta value for RoPE.")
 
     # gradient_clipping
@@ -37,11 +48,24 @@ def main():
     parser.add_argument("--T_w", type=int, default=4, help="Number of layers for Transformer.")
     parser.add_argument("--T_c", type=int, default=16, help="Number of heads for Transformer.")
 
+    # training loop
+    parser.add_argument("--epochs", type=int, default=10000, help="Number of epochs to train")
+
+    # save checkpoint args
+    checkpoint_uuid = uuid4().hex[:9]
+    logger.info(f"Using {checkpoint_uuid} as unique prefix if no checkpoint prefix is provided")
+    parser.add_argument("--checkpoint-dir", type=str, default="./checkpoints", help="directory to save checkpoints in")
+    parser.add_argument("--checkpoint-prefix", type=str, default=f"run-{checkpoint_uuid}-checkpoint-")
+
+    # load checkpoint args
+    parser.add_argument("--load-checkpoint-from", type=str, help="If provided, load checkpoint from this filepath")
+
     args = parser.parse_args()
 
+    dataset = np.load(args.dataset, mmap_mode="r")
 
     training_loader = data_loading(
-        dataset=args.dataset,
+        dataset=dataset,
         batch_size=args.batch_size,
         context_length=args.context_length,
         device=args.device,
@@ -53,7 +77,7 @@ def main():
     )
 
     model = TransformerLM(
-        vocab_size=args.vocab_size, 
+        vocab_size=args.vocab_size,
         context_length=args.context_length,
         d_model=args.d_model,
         num_layers=args.num_layers,
@@ -64,13 +88,17 @@ def main():
 
     loss_fn = cross_entropy
 
-    return training_loader, optimizer, model, loss_fn, args
+    if args.load_checkpoint_from is not None:
+        last_epoch_index = load_checkpoint(args.load_checkpoint_from, model, optimizer)
+    else:
+        last_epoch_index = 0
+
+    return training_loader, optimizer, model, loss_fn, last_epoch_index, args
 
 
-
-def train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, loss_fn, args):
-    running_loss = 0.
-    last_loss = 0.
+def train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, loss_fn, wandb_run, args):
+    running_loss = 0.0
+    last_loss = 0.0
 
     # Here, we use enumerate(training_loader) instead of
     # iter(training_loader) so that we can track the batch
@@ -80,7 +108,7 @@ def train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, l
         inputs, targets = data
 
         # Zero your gradients for every batch!
-        optimizer.zero_grad() 
+        optimizer.zero_grad()
 
         # Make predictions for this batch
         logits = model(inputs)
@@ -88,36 +116,38 @@ def train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, l
         # Compute the loss and its gradients
         loss = loss_fn(logits, targets)
         loss.backward()
-        
-        
+
         apply_gradient_clipping(model.parameters(max), max_l2_norm=args.max_l2_norm, eps=args.eps)
 
         get_learning_rate_schedule(t=i, a_max=args.a_max, a_min=args.a_min, T_w=args.T_w, T_c=args.T_c)
         # Adjust learning weights
         optimizer.step()
-        
-
-        # Save Checkpoint
-        save_checkpoint(
-            model=model,
-            optimizer=optimizer,
-            iteration=i,
-            #todo: out=??? 
-        )
 
         # Gather data and report
         running_loss += loss.item()
+        wandb_run.log({"loss": loss})
         if i % 1000 == 999:
-            last_loss = running_loss / 1000 # loss per batch
-            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            last_loss = running_loss / 1000  # loss per batch
+            print("  batch {} loss: {}".format(i + 1, last_loss))
             tb_x = epoch_index * len(training_loader) + i + 1
-            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-            running_loss = 0.
+            tb_writer.add_scalar("Loss/train", last_loss, tb_x)
+            running_loss = 0.0
+
+    save_checkpoint(
+        model=model,
+        optimizer=optimizer,
+        iteration=epoch_index,
+        out=f"{args.checkpoint_dir}/{args.checkpoint_prefix}{i}.pt",
+    )
 
     return last_loss
 
 
-
 if __name__ == "__main__":
-    training_loader, optimizer, model, loss_fn = main()
-    train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, loss_fn)
+    training_loader, optimizer, model, loss_fn, last_epoch_index, args = main()
+    tb_writer = SummaryWriter()
+    wandb_run = wandb.init(project="mini-llm")
+    wandb_run.watch(model)
+
+    for epoch_index in range(last_epoch_index, args.epochs):
+        train_one_epoch(epoch_index, tb_writer, training_loader, optimizer, model, loss_fn, wandb_run, args)
